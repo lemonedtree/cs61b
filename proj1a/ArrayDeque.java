bf45033d2d49b73bf90c204d01c4fabcb3f9871b
lemonedtree <1096871337@qq.com>
2021-11-27 19:23:23 +0800

    project 1A 1.0

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,5 @@
+package PACKAGE_NAME;/**
+@author XD
+@create 2021-11-26 19:07
+*/public class ArrayDeque {
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,5 @@
+package PACKAGE_NAME;/**
+@author XD
+@create 2021-11-25 20:25
+*/public class LinkedListDeque {
+}
