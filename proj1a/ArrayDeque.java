ece6ad216e9347c15a347eba4aee9e3e9fe87f13
lemonedtree <1096871337@qq.com>
2021-11-28 11:57:08 +0800

    project 1A 2.4 modify the remove()

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -175,7 +175,7 @@ public class ArrayDeque<T> {
         nextLast = toLast();
 
         //downsize
-        if (size * 1.0 / (items.length) < 0.25) {
+        if (size * 1.0 / (items.length) < 0.25 && items.length >= 16) {
             resize(size / 2);
         }
 
