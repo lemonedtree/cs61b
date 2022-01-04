d3c23c3b3d17fcfc85e21e9d3ba2a64a78f7662e
lemonedtree <1096871337@qq.com>
2022-01-04 20:45:19 +0800

    hw2 1.0

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,136 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    // create N-by-N grid, with all sites initially blocked
+    private int N;
+    private WeightedQuickUnionUF w;
+    private int numberOfOpenSites;
+    private boolean[] opens;
+    private int[] topRow;
+    private int[] bottomRow;
+    
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException("N should not less than 0!");
+        }
+        this.N = N;
+        w = new WeightedQuickUnionUF(N * N);
+        opens = new boolean[N * N];
+        numberOfOpenSites = 0;
+        topRow = new int[N];
+        bottomRow = new int[N];
+        for (int i = 0; i < N; i++) {
+            topRow[i] = i;
+            bottomRow[i] = N * (N - 1) + i;
+        }
+    }
 
+    private int findXY(int row, int col) {
+        int xy = (N * row + col);
+        return xy;
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (row >= N || col >= N || row < 0 || col < 0) {
+            throw new java.lang.IndexOutOfBoundsException("There is no such position!");
+        }
+
+
+        if (isOpen(row,col) == false) {
+            //记录open状态
+            opens[findXY(row, col)] = true;
+            numberOfOpenSites++;
+            //把它和它的邻居连接起来
+            int[] neighbors = findOpenNeighbors(row,col);
+            int xy = findXY(row,col);
+            for (int i = 0; i < 4; i++) {
+                //首先要存在邻居
+                if (neighbors[i] != -1) {
+                    //如果本来没有连接，就连接起来
+                    if (w.connected(neighbors[i],xy) == false) {
+                        w.union(neighbors[i],findXY(row,col));
+                    }
+                }
+            }
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row >= N || col >= N || row < 0 || col < 0) {
+            throw new java.lang.IndexOutOfBoundsException("There is no such position!");
+        }
+        return opens[findXY(row,col)];
+    }
+
+    /**
+     * 数组中，如果有开了的邻居，就返回邻居的代码，如果没有开了的邻居，就返回-1
+     * @param row
+     * @param col
+     * @return int[] neighbors
+     */
+    private int[] findOpenNeighbors(int row,int col) {
+        int[] neighbors = new int[4];
+        //上面那个邻居
+        if (row == 0 || isOpen(row - 1,col) == false) {
+            neighbors[0] = -1;
+        } else {
+            neighbors[0] = findXY(row - 1, col);
+        }
+        //下面那个邻居
+        if (row == N - 1 || isOpen(row + 1,col) == false) {
+            neighbors[1] = -1;
+        } else {
+            neighbors[1] = findXY(row + 1, col);
+        }
+        //左边那个邻居
+        if (col == 0  || isOpen(row,col - 1) == false) {
+            neighbors[2] = -1;
+        } else {
+            neighbors[2] = findXY(row, col - 1);
+        }
+        //右边那个邻居
+        if (col == N - 1  || isOpen(row,col + 1) == false) {
+            neighbors[3] = -1;
+        } else {
+            neighbors[3] = findXY(row, col + 1);
+        }
+        return neighbors;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col)  {
+        if (row >= N || col >= N || row < 0 || col < 0) {
+            throw new java.lang.IndexOutOfBoundsException("There is no such position!");
+        }
+
+        boolean flag = false;
+        for (int i:topRow) {
+            if (w.connected(i,findXY(row, col))) {
+                flag = true;
+                break;
+            }
+        }
+        return (flag && isOpen(row,col));
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        boolean flag = false;
+        for (int i:topRow) {
+            for (int j:bottomRow) {
+                if (w.connected(i,j)){
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,55 @@
 package hw2;
 
 public class PercolationStats {
+        // perform T independent experiments on an N-by-N grid
+        int N;
+        int T;
+        int[] openSitesRecord;
 
+        public PercolationStats(int N, int T, PercolationFactory pf) {
+            this.N = N;
+            this.T = T;
+            openSitesRecord = new int[T];
+            pf = new PercolationFactory();
+            for (int i = 0; i < T; i++) {
+                Percolation p = pf.make(N);
+                while (!p.percolates()) {
+                    //random是0-1之间的浮点数， 我要的是 0 - N 之间的整数
+                    int row = (int) Math.random() * N;
+                    int col = (int) Math.random() * N;
+                    p.open(row,col);
+                }
+                openSitesRecord[i] = p.numberOfOpenSites();
+            }
+        }
+
+        // sample mean of percolation threshold
+        public double mean() {
+            double sum = 0;
+            for (int i = 0; i < T; i++) {
+                sum += openSitesRecord[i];
+            }
+            return (sum / T);
+        }
+
+        // sample standard deviation of percolation threshold
+    //应该还要开个根号，妈的
+        public double stddev() {
+            double mean = mean();
+            double sum2 = 0;
+            for (int i = 0; i < T; i++) {
+                sum2 = sum2 + ((openSitesRecord[i] - mean) * (openSitesRecord[i] - mean));
+            }
+            return Math.sqrt((sum2 / (T - 1)));
+        }
+
+        // low endpoint of 95% confidence interval
+        public double confidenceLow() {
+            return mean() - 1.96 * stddev() / Math.sqrt(T);
+        }
+
+        // // high endpoint of 95% confidence interval
+        public double confidenceHigh() {
+            return mean() + 1.96 * stddev() / Math.sqrt(T);
+        }
 }
